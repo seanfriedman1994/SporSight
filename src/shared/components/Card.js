8ad5cb479977ea97952ea02e2b990756d9772281
no <no@no.no>
2020-05-27 23:30:24 -0400

    Attempt to move files to new src folder

--- src/shared/components/Card.js
+++ src/shared/components/Card.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import {View, StyleSheet} from 'react-native';
 
+// TODO: Delete?
+
 const Card = props => {
     return (
         <View style={{...styles.card, ...props.style}}>{props.children}</View>
